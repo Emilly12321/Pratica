d49b5370984120383d28f9254d1b9734a458aed5
Emilly12321 <emillysilvaluz2839@gmail.com>
2025-09-04 21:40:45 -0300

    fix main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 function ValidaCampos() {
-    
-    var expreRegNome = new RegExp("^[A-zÁ-ü]{3,}([ ][A-zÁ-ü]{2,})+$");
+
+    var expreRegNome = new RegExp("^[A-zÁ-ü]{3,}(([ ][A-zÁ-ü]{2,})?)+$");
     var expreRegTel = new RegExp("^[+][0-9]{2}[ ]{1}[(][0-9]{2}[)][ ]{1}[0-9]{4,5}[-][0-9]{4}$");
-    var expreRegEmail = new RegExp("^[A-z0-9]+[@](gmail|outlook|estudante)(\\.com|\\.sesisenai\\.br)");
-    
+    var expreRegEmail = new RegExp("^([A-z0-9]{2,})+[@](gmail|outlook|estudante)(\.com|\.sesisenai)([\.][br])?$");
+    var expreRegCPF = new RegExp("^[0-9]{3}[\.][0-9]{3}[\.][0-9]{3}[-][0-9]{2}$");
+
     let nome = document.getElementById('nome').value;
     let telefone = document.getElementById('fone').value;
     let email = document.getElementById('email').value;
+    let cpf = document.getElementById('cpf').value;
 
 
 
@@ -22,9 +24,16 @@ function ValidaCampos() {
         alert("Favor preencha com um email válido!");
         document.getElementById('email').focus();
         return false;
+    } else if (!expreRegCPF.test(cpf)) {
+        alert("Favor preencha com um cpf válido!");
+        document.getElementById('cpf').focus();
+        return false;
+    } else {
+
+        alert("Enviado com sucesso")
+        console.log(nome + telefone + email + cpf);
+        return false;
     }
 
-    
-    console.log(nome + telefone + email);   
     return false;
 }
